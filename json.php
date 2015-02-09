6780fde602d5841551d19db84434602c564dd3e4
Bruno Eris <brunoeriss@gmail.com>
2015-02-09 11:00:44 -0300

    Identação e correções

--- json.php
+++ json.php
@@ -10,27 +10,6 @@ while($r = mysql_fetch_assoc($result)) {
     $rows[] = $r;
 }
 
-// ##### Modo Estático #####
-/* array referente a 3 pessoas
-    $var = Array(
-    array(
-        "nome"=>"João",
-        "sobreNome"=>"Silva",
-        "cidade"=>"Maringá"
-    ),
-    array(
-        "nome"=>"Ana",
-        "sobreNome"=>"Rocha",
-        "cidade"=>"Londrina"
-    ),
-    array(
-        "nome"=>"Véra",
-        "sobreNome"=>"Valério",
-        "cidade"=>"Cianorte"
-    ));
-*/
-// convertemos em json e colocamos na tela
     echo json_encode($rows);
 
-    //fecha a conexão com o banco
     mysql_close();
